817fe2be0c78903d00cdb090b42df98b21dbc1b0
Vision-VLSI Industry Training Institute <vvi.pvtltd@gmail.com>
2023-05-28 13:15:03 +0530

    Create class_ex1.sv

---
+++ sv_oops/class_ex1.sv
@@ -0,0 +1,21 @@
+```
+class first;
+  
+  reg [2:0] data; 
+  reg [1:0] data2;
+
+endclass
+
+module tb;
+  
+  first f;
+
+  initial begin
+    f = new(); 
+    #1;
+    $display("Value of data : %0d and data2 : %0d",f.data, f.data2);
+  end
+
+endmodule
+
+```
